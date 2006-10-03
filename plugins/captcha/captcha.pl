c9f655fa0373ffe8625efe8477364f5de7094e3b
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2006-10-03 08:57:03 +0000

    * Now can select enable or disable Captcha test per-blog basis * Now can change the length of Captcha * Now can be set a secret key for generating Captcha * Captcha's output_folder can be placed under user-specific directory.  Because, one can make plugins directory be executable but not readable (it might be usual for most web-hosting services?).
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-Captcha/trunk@7 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- plugins/captcha/captcha.pl
+++ plugins/captcha/captcha.pl
@@ -15,26 +15,33 @@ use File::Spec;
 use File::Basename;
 use Authen::Captcha;
 use base 'MT::Plugin';
-our $VERSION = '0.02';
+our $VERSION = '0.10';
+
+my $dirname = dirname(__FILE__);
+my $cgipath = MT::ConfigMgr->instance->CGIPath;
+$cgipath .= '/' unless $cgipath =~ m!/$!;
 
 my $plugin = __PACKAGE__->new({
     name => 'Captcha',
     description => 'This plugin enables comment-posting with CAPTCHA test.',
-    doc_link => 'http://as-is.net/wiki/Captcha_Plugin',
+    doc_link => 'http://code.as-is.net/wiki/Captcha_Plugin',
     author_name => 'Hirotaka Ogawa',
     author_link => 'http://profile.typekey.com/ogawa/',
     version => $VERSION,
-    blog_config_template => \&config_tmpl,
+    blog_config_template => 'config.tmpl',
     settings => new MT::PluginSettings([
+	['captcha_enable', { Default => 0 }],
 	['captcha_ttl', { Default => 3600 }],
+	['captcha_secret', { Default => '' }],
+	['captcha_length', { Default => 5 }],
+	['captcha_images_url', { Default => $cgipath . 'plugins/captcha/images/' }],
+	['captcha_images_path', { Default => File::Spec->catdir($dirname, 'images') }],
     ]),
 });
 MT->add_plugin($plugin);
 
-my $dirname = dirname(__FILE__);
 my $captcha = Authen::Captcha->new(
-   data_folder => File::Spec->catdir($dirname, 'data'),
-   output_folder => File::Spec->catdir($dirname, 'images')
+   data_folder => File::Spec->catdir($dirname, 'data')
 );
 
 MT->add_callback('CommentThrottleFilter', 5, $plugin, \&captcha_test);
@@ -43,6 +50,8 @@ MT::Template::Context->add_tag(CaptchaJsURL => \&captcha_js_url);
 sub captcha_test {
     my ($eh, $app, $entry) = @_;
     my $blog = $entry->blog;
+
+    # check if commenter is logged on to typekey
     if ($blog->allow_reg_comments && $blog->effective_remote_auth_token) {
 	my ($session_key, $commenter) = $app->_get_commenter_session();
 	return 1 if $commenter;
@@ -52,7 +61,17 @@ sub captcha_test {
     my $code = $q->param('captcha_code') or return 0;
     my $md5 = $q->param('captcha_md5') or return 0;
 
-    my $ttl = config('blog:' . $blog->id)->{captcha_ttl};
+    # load config
+    my $cfg = config('blog:' . $blog->id);
+
+    # check if captcha-test disabled
+    return 1 unless $cfg->{captcha_enable};
+
+    # configure Auth::Captcha
+    $captcha->output_folder($cfg->{captcha_images_path});
+    $captcha->secret($cfg->{captcha_secret} || '');
+
+    my $ttl = $cfg->{captcha_ttl};
     $ttl = 3600 if $ttl !~ /^\d+$/;
     $captcha->expire($ttl);
 
@@ -61,9 +80,48 @@ sub captcha_test {
 
 sub captcha_js_url {
     my ($ctx, $args) = @_;
+    my $blog_id = $ctx->stash('blog')->id;
     my $path = MT::ConfigMgr->instance->CGIPath;
     $path .= '/' unless $path =~ m!/$!;
-    $path . 'plugins/captcha/captcha_js.cgi';
+    $path . 'plugins/captcha/captcha_js.cgi?blog_id=' . $blog_id;
+}
+
+sub dump_settings {
+    my $plugin = shift;
+
+    my $conf = '';
+    my @blogs = MT::Blog->load;
+    for my $blog (@blogs) {
+	my $config = $plugin->get_config_hash('blog:' . $blog->id);
+	next unless $config->{captcha_enable};
+	$conf .= $blog->id . ',' .
+	    $config->{captcha_ttl} . ',' .
+	    $config->{captcha_secret} . ',' .
+	    $config->{captcha_length} . ',' .
+	    $config->{captcha_images_url} . ',' .
+	    $config->{captcha_images_path} . "\n";
+    }
+
+    my $cfg_file = File::Spec->catfile(dirname(__FILE__), 'data', 'config.txt');
+    local(*FH);
+    open FH, ">$cfg_file" or die "Can't open File: $cfg_file\n";
+    flock FH, 2;
+    print FH $conf;
+    close(FH);
+}
+
+sub save_config {
+    my $plugin = shift;
+    $plugin->SUPER::save_config(@_);
+    delete $plugin->{__config_obj}{$_[0]}; # invalidate cache
+    $plugin->dump_settings();
+}
+
+sub reset_config {
+    my $plugin = shift;
+    $plugin->SUPER::reset_config(@_);
+    delete $plugin->{__config_obj}{$_[0]}; # invalidate cache
+    $plugin->dump_settings();
 }
 
 use MT::Request;
@@ -77,17 +135,6 @@ sub config {
 	$r->cache('captcha_config_' . $scope, $cfg);
     }
     $cfg;
-}
-
-sub config_tmpl {
-    my $tmpl = <<'EOT';
-<div class="setting">
-  <div class="label"><label for="captcha_ttl"><MT_TRANS phrase="CAPTCHA TTL:"></label></div>
-  <div class="field">
-    <input name="captcha_ttl" id ="captcha_ttl" size="5" value="<TMPL_VAR NAME=CAPTCHA_TTL ESCAPE=HTML>" /> <MT_TRANS phrase="seconds"> (max: 3600 seconds)
-  </div>
-</div>
-EOT
 }
 
 1;
--- plugins/captcha/captcha_js.cgi
+++ plugins/captcha/captcha_js.cgi
@@ -3,22 +3,65 @@
 # $Id$
 #
 use Authen::Captcha;
+use File::Spec;
+use File::Basename;
 use CGI;
 
-my $captcha = Authen::Captcha->new(data_folder => './data',
-				   output_folder => './images',
-				   expire => 3600);
-my $captcha_length = 5;
-my $captcha_img_width = 25 * $captcha_length;
-my $captcha_img_height = 35;
+my $dirname = dirname(__FILE__);
+my $captcha = Authen::Captcha->new(
+   data_folder => File::Spec->catdir($dirname, 'data')
+);
+my $cfg_file = File::Spec->catfile($dirname, 'data', 'config.txt');
+my $config;
 
 my $q = CGI->new;
-gen_code($q);
+eval {
+    $config = read_config($cfg_file);
+    gen_code($q);
+};
+if ($@) {
+    print $q->header("text/plain"), $@ if $@;
+}
+
+sub read_config {
+    my($cfg_file) = @_;
+    my $config;
+
+    local(*FH, $_, $/);
+    $/ = "\n";
+    open FH, $cfg_file or die "Can't open File: $cfg_file\n";
+    flock FH, 1;
+    while (<FH>) {
+	chomp;
+	my @c = split(',', $_);
+	next unless scalar @c == 6;
+	$config->{$c[0]} = {
+	    captcha_ttl => $c[1],
+	    captcha_secret => $c[2],
+	    captcha_length => $c[3],
+	    captcha_images_url => $c[4],
+	    captcha_images_path => $c[5],
+	};
+    }
+    close(FH);
+    $config;
+}
 
 sub gen_code {
     my $q = shift;
-    my $url = $q->url;
-    $url =~ s!/[^/]+$!!;
+    my $blog_id = $q->param('blog_id') || 1;
+    my $conf = $config->{$blog_id} or die "blog_id is not properly given.";
+
+    $captcha->expire($conf->{captcha_ttl} || 3600);
+    $captcha->secret($conf->{captcha_secret} || '');
+    $captcha->output_folder($conf->{captcha_images_path});
+
+    my $captcha_images_url = $conf->{captcha_images_url};
+    $captcha_images_url .= '/' if $captcha_images_url !~ m!/$!;
+
+    my $captcha_length = $conf->{captcha_length} || 5;
+    my $captcha_img_width = 25 * $captcha_length;
+    my $captcha_img_height = 35;
 
     my $captcha_md5 = $captcha->generate_code($captcha_length);
 
@@ -28,7 +71,7 @@ if (!commenter_name) {
   document.writeln('<div id="comment-captcha-block">');
   document.writeln('<input type="hidden" name="captcha_md5" value="$captcha_md5" />');
   document.writeln('<label for="comment-captcha">CAPTCHA&trade; Code:</label>');
-  document.writeln('<img src="$url/images/$captcha_md5.png" width="$captcha_img_width" height="$captcha_img_height" alt="CAPTCHA Image" />');
+  document.writeln('<img src="$captcha_images_url$captcha_md5.png" width="$captcha_img_width" height="$captcha_img_height" alt="CAPTCHA Image" />');
   document.writeln('<input type="text" id="comment-captcha" name="captcha_code" value="" length="$captcha_length" maxlength="$captcha_length" />');
   document.writeln('</div>');
 }
